3fe44f7c98e2fd1a0fd23d2c90aacf41ae277e2a
shoulansi <garrettswhite@gmail.com>
2025-03-19 19:02:16 -0500

    final arrays

--- practice-creating-arrays-1.js
+++ practice-creating-arrays-1.js
@@ -1,1 +1,12 @@
+let dairyProducts = ["1% Milk", "Eggs", "Cheddar Cheese", "Yogurt", "Coffee Creamer"];
+let prices = [10.99, 39.99, 59.99];
+let climbingItems = ["Climbing Shoes", "Chalk", "Climbing Tape"];
 
+console.log(dairyProducts);
+console.log(prices);
+console.log(climbingItems);
+console.log(dairyProducts[3])
+console.log(dairyProducts[0])
+console.log(dairyProducts[2])
+console.log(prices[0])
+console.log(prices[2])
